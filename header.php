2c8b054a7873808c7374d94d35b85714b4e91ae3
Brett Spencer <bspencer411@gmail.com>
2015-11-23 22:30:11 -0800

    removed header profile link

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <link rel='stylesheet' id='fontAwesome' href='http://robinsalas.com/wp-content/themes/sela-child/css/font-awesome.css' type='text/css' media='all' />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
 </head>
